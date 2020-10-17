4adb7a2686d5183fec61bc23b3f7bece0cd6b091
ANIRUDHA KULKARNI <kulkarnianirudha8@gmail.com>
2020-10-17 22:06:04 +0530

    major update

--- imgs/app.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $("[type = submit]").click(function() {
-    var comment = $("[type = text]").val();
-    $("<p lang=" + ">" + comment + "</p>").appendTo("body");
-  });
-});
